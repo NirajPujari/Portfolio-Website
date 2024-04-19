180b41eb3fd5fbe0773753201f7235f820659866
NirajPujari <pujari.niraj09@gmail.com>
2024-04-20 00:44:41 +0530

    changes in the css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
-  return (
-    <main className="">
-      
-    </main>
-  );
+	return (
+		<main>
+			<div className="apps">Hello World</div>
+			<div className="appss">Hello World</div>
+			<div className="aspps">Hello World</div>
+		</main>
+	);
 }
